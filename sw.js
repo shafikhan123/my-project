fb8c78a5abeba71b0a90a92a52b601687125d5eb
SK <SK>
2017-11-26 20:23:31 +0530

    added files

--- sw.js
+++ sw.js
@@ -2,9 +2,12 @@
 
 importScripts('sw-toolbox.js');
 
-toolbox.precache(["index.html", "elements2.html", "generic.html", "style/*", "script/*", "fonts/*"]);
+toolbox.precache(["index.html", "gene.html"]);
 
 toolbox.router.get('/images/*', toolbox.cacheFirst);
+toolbox.router.get('/style/*', toolbox.cacheFirst);
+toolbox.router.get('/fonts/*', toolbox.cacheFirst);
+toolbox.router.get('/script/*', toolbox.cacheFirst);
 
 toolbox.router.get('/*', toolbox.networkFirst, {
   networkTimeoutSeconds: 5
