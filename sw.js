71d42253dce9424abbbc1010189629f235b6af97
SK <SK>
2017-11-26 14:52:13 +0530

    Pushed sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
 'use strict';
 importScripts('sw-toolbox.js');
-toolbox.precache('index.html','style/style.css']); 
-toolbox.router.get('/images/*', toolbox.cacheFirst); toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
+toolbox.precache(['index.html','style/style.css']); 
+toolbox.router.get('/images/*', toolbox.cacheFirst);
+toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
