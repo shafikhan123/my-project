128365129f75fb367012c1b612d38be3b8e9a0bc
SK <SK>
2017-11-26 16:45:52 +0530

    added files

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 
 importScripts('sw-toolbox.js');
 
-toolbox.precache(["index.html","style/style.css"]);
+toolbox.precache(["index.html","elements.html", "style/style.css"]);
 
 toolbox.router.get('/images/*', toolbox.cacheFirst);
 
